bce677eec38ba9fc38a9ecac0997516f952b5cf7
hppyHNGRYslpy <thomas.sanger92@gmail.com>
2021-12-27 09:16:12 -0500

    Added .cpp

---
+++ DataTypes.cpp
@@ -0,0 +1,53 @@
+// HEADER
+/*Student Name: Thomas Sanger
+Lab Assignment: Lab01_01
+Project Name: Restuarant Bill
+File Name: RestuarantBill.cpp
+Description: Calculate the tax and tip of a dinner bill
+Limitations or issues: Fixed values only, no user input
+Credits: Instructor walked us through this in lab
+*/
+
+// Preprocessor directive placed here
+#include <iostream>
+using namespace std;
+
+// Function prototypes placed here
+
+// External variable declarations placed here
+
+// Global variable declarations placed here
+
+// Global constants declarations placed here
+
+int main()
+{
+	// program introduction
+	cout << "This program outputs data type sizes.\n" << endl;
+
+	// print statements
+	cout << "Data Type\tSize" << endl;
+	cout << "---------\t----" << endl;
+	cout << "char\t\t" << sizeof(char) << endl;
+	cout << "int\t\t" << sizeof(int) << endl;
+	cout << "float\t\t" << sizeof(float) << endl;
+	cout << "double\t\t" << sizeof(double) << endl;
+
+	system("pause");
+	return 0;
+}
+
+/*
+PROOF:
+
+This program outputs data type sizes.
+
+Data Type       Size
+---------       ----
+char            1
+int             4
+float           4
+double          8
+Press any key to continue . . .
+
+*/
